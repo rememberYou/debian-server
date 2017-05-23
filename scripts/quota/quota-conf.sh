40934211605b8880ccc2700fda37b24085201f49
Terencio Agozzino <terencio.agozzino@gmail.com>
2017-05-23 15:34:39 +0200

    Script: Quota should be fixed now

--- scripts/quota/quota-conf.sh
+++ scripts/quota/quota-conf.sh
@@ -5,14 +5,26 @@
 # Installation of quotatool, useful for scripts.
 apt-get install quota quotatool
 
-# Add this to /etc/fstab to the /home line
+# Unmout the /home partition.
+user -k /dev/mapper/VolGroup-LVhome
+umount -l /dev/mapper/VolGroup-LVhome
+
+# Unmout the /srv/share partition
+user -k /srv/share
+umount -l /srv/share
+
+# Add this to /etc/fstab to the /home and /srv/share line
 usrquota,grpquota
 
-quotacheck -cguvf /dev/mapper/VolGroup-LVhome
-# quotacheck -cguvf /srv/share
+# Create the file 'aquota.user' and aquota.group' and initialize all the
+# partitions that contains quotas in the /etc/fstab.
+quotacheck -cagumv
 
-# Initialize all the partitions that contains quotas in the /etc/fstab.
-quotacheck -vagum
+# Mount the /home partition.
+mount /dev/mapper/VolGroup-LVhome
+
+# Mount the srv/share partition.
+mount srv/share
 
 # Activate quota
 quotaon -avug
