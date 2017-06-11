05aa32df3aaf0f16ea6e75c81a093c7cae4d53f8
Harchytekt <alexandre.ducobu@yahoo.be>
2017-06-11 11:53:01 +0200

    some changes in the scripts

--- scripts/apache/apache_client-conf.sh
+++ scripts/apache/apache_client-conf.sh
@@ -6,9 +6,9 @@ function create_website() {
     echo -e "\tServerAdmin $username@$username.lan" >> "/etc/apache2/sites-enabled/$username.lan.conf"
     echo -e "\tServerName $username.lan" >> "/etc/apache2/sites-available/$username.lan.conf"
     echo -e "\tServerAlias www.$username.lan" >> "/etc/apache2/sites-available/$username.lan.conf"
-    echo -e "\tDocumentRoot /srv/web/$username/www" >> "/etc/apache2/sites-enabled/$username.lan.conf"
-    echo -e "\tErrorLog /srv/web/$username.lan/logs/error.log" >> "/etc/apache2/sites-enabled/$username.lan.conf"
-    echo -e "\tCustomLog /srv/web/$username.lan/logs/access.log combined" >> "/etc/apache2/sites-enabled/$username.lan.conf"
+    echo -e "\tDocumentRoot /srv/www/$username/www" >> "/etc/apache2/sites-enabled/$username.lan.conf"
+    echo -e "\tErrorLog /srv/www/$username.lan/logs/error.log" >> "/etc/apache2/sites-enabled/$username.lan.conf"
+    echo -e "\tCustomLog /srv/www/$username.lan/logs/access.log combined" >> "/etc/apache2/sites-enabled/$username.lan.conf"
     echo "</VirtualHost>" >> "/etc/apache2/sites-enabled/$username.lan.conf"
 }
 
--- scripts/http/httpd-conf.sh
+++ scripts/http/httpd-conf.sh
@@ -6,7 +6,7 @@ echo "<VirtualHost 10.1.215.223:80>" >> "/etc/apache2/sites-available/$1.com.con
 echo "     ServerAdmin $1@$1.com" >> "/etc/apache2/sites-available/$1.com.conf"
 echo "     ServerName $1@$1.com" >> "/etc/apache2/sites-available/$1.com.conf"
 echo "     ServerAlias www.$1.com" >> "/etc/apache2/sites-available/$1.com.conf"
-echo "     DocumentRoot \"/srv/web/$1/www\"" >> "/etc/apache2/sites-available/$1.com.conf"
+echo "     DocumentRoot \"/srv/www/$1/www\"" >> "/etc/apache2/sites-available/$1.com.conf"
 echo "     ErrorLog ${APACHE_LOG_DIR}/error.log" >> "/etc/apache2/sites-available/$1.com.conf"
 echo "     CustomLog ${APACHE_LOG_DIR}/access.log combined" >> "/etc/apache2/sites-available/$1.com.conf"
 echo "</VirtualHost>"
